43641912d1e94f17eeb8aeb01a80af3fa46e1bfe
UtescherIntrieri <18ianaui1999@gmail.com>
2023-12-18 20:48:53 -0300

    Aula 2

---
+++ script.js
@@ -0,0 +1,18 @@
+let contactList = [
+  {
+    'name' : 'Maria Silva',
+    'email' : 'maria@gmail.com',
+    'city' : 'Cajamar', 
+    'state' : 'SP',
+    'country' : 'Brasil',
+    'picture': 'https://randomuser.me/api/portraits/women/43.jpg'
+  },
+  {
+    'name' : 'Roberta Silva',
+    'email' : 'roberta@gmail.com',
+    'city' : 'São Paulo', 
+    'state' : 'SP',
+    'country' : 'Brasil',
+    'picture': 'https://randomuser.me/api/portraits/women/45.jpg'
+  },
+]
